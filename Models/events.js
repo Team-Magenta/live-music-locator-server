227c1c39d23320b51135106262c57867804b9884
tempusedax <105181037+tempusedax@users.noreply.github.com>
2022-07-07 16:48:18 -0500

    Revert "crud"

--- Models/events.js
+++ Models/events.js
@@ -5,12 +5,10 @@ const { Schema } = mongoose;
 
 const eventSchema = new Schema({
 
-
-    artist: { type: String, required: true },
     image: { type: String, required: true },
-    venue: { type: Array, required: true },
-    attended: {type: Boolean, default: false, required: false},
-    date: { type: String, required: true}
+    venue: { type: String, required: true },
+    artist: { type: String, required: true },
+    date: { type: String, required: true }
 
 });
 
